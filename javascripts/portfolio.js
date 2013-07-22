fe49d3814b41f9fa328203f6c10f9d0da4292c09
alexdavidow <alex.davidow@gmail.com>
2013-07-21 21:39:45 -0400

    trying to setup updated github io page

---
+++ javascripts/portfolio.js
@@ -0,0 +1,51 @@
+function show_port1(){
+  $('.intro').hide("scale", 1000);
+  $('#port_1').fadeIn(1100);
+  $('#port_2').fadeIn(1100);
+  // $('#contact').hide("scale", 1000);
+}
+
+function about(){
+  $('#port_1').hide("scale", 1000);
+  $('#port_2').hide("scale", 1000);
+  // $('#contact').hide("scale", 1000);
+  $('.intro').show("scale", 1000);
+  $('.ui-dialog').hide("scale", 1000);
+}
+
+// function contact() {
+//   $('#port_1').hide("scale", 1000);
+//   $('#port_2').hide("scale", 1000);
+//   $('.ui-dialog').hide("scale", 1000);
+//   $('.intro').hide("scale", 1000);
+//   $("#contact").fadeIn(1000);
+// }
+
+$(document).ready(function(){
+  $('#cupcake_screenshot').attr('src', "images/cupcakegame_screenshot.png");
+  $('#worldcup_screenshot').attr('src', "images/worldcupapp_screenshot.png");
+  
+  $('.hidden').hide();
+  
+  $('#portfolio_nav').on('click', show_port1);
+  $('#about_nav').on('click', about);
+  // $('#contact_nav').on('click', contact);
+  
+  $("#dialog_cupcake").dialog({autoOpen: false});
+  // $("#dialog_cupcake").dialog({ position: {my: "center bottom", at: "center top", of: "#cupcake_screenshot"}});
+  $("#cupcake_screenshot").on('mouseover', function(){   
+    $("#dialog_cupcake").dialog( "close"); 
+    $("#dialog_cupcake").dialog( "option", "position", { my: "left top", at: "left bottom", of: $("#cupcake_screenshot") } );
+    $("#dialog_cupcake").dialog( "open");
+  });
+
+  $("#dialog_worldcup").dialog({autoOpen: false});
+  $("#worldcup_screenshot").on('mouseover', function(){
+    $("#dialog_worldcup").dialog( "close"); 
+    $("#dialog_worldcup").dialog( "option", "position", { my: "left top", at: "left bottom", of: $("#worldcup_screenshot") } );
+    $("#dialog_worldcup").dialog("open");
+  });
+
+
+  // $('#big-image').attr('src', 'http://www.nasa.gov/centers/goddard/images/content/433226main_Misti_ComaCluster.jpg');
+});
