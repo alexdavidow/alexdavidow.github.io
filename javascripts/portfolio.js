36bda0a39a958b5633f05c4e74eec04c8376e272
alexdavidow <alex.davidow@gmail.com>
2013-08-15 17:00:47 -0400

    added mouseleave to dialog boxes and made name clickable link

--- javascripts/portfolio.js
+++ javascripts/portfolio.js
@@ -1,16 +1,16 @@
 function show_port1(){
-  $('.intro').hide("scale", 1000);
+  $('.intro').hide("scale", 800);
   $('#port_1').fadeIn(1100);
   $('#port_2').fadeIn(1100);
   // $('#contact').hide("scale", 1000);
 }
 
 function about(){
-  $('#port_1').hide("scale", 1000);
-  $('#port_2').hide("scale", 1000);
+  $('#port_1').hide("scale", 800);
+  $('#port_2').hide("scale", 800);
   // $('#contact').hide("scale", 1000);
   $('.intro').show("scale", 1000);
-  $('.ui-dialog').hide("scale", 1000);
+  $('.ui-dialog').hide("scale", 800);
 }
 
 // function contact() {
@@ -29,7 +29,7 @@ $(document).ready(function(){
   
   $('#portfolio_nav').on('click', show_port1);
   $('#about_nav').on('click', about);
-  // $('#contact_nav').on('click', contact);
+  $('#name').on('click', about);
   
   $("#dialog_cupcake").dialog({autoOpen: false});
   // $("#dialog_cupcake").dialog({ position: {my: "center bottom", at: "center top", of: "#cupcake_screenshot"}});
@@ -38,12 +38,18 @@ $(document).ready(function(){
     $("#dialog_cupcake").dialog( "option", "position", { my: "left top", at: "left bottom", of: $("#cupcake_screenshot") } );
     $("#dialog_cupcake").dialog( "open");
   });
+  $("#dialog_cupcake").on('mouseleave', function(){
+    $("#dialog_cupcake").dialog( "close");
+  });
 
   $("#dialog_worldcup").dialog({autoOpen: false});
   $("#worldcup_screenshot").on('mouseover', function(){
     $("#dialog_worldcup").dialog( "close"); 
     $("#dialog_worldcup").dialog( "option", "position", { my: "left top", at: "left bottom", of: $("#worldcup_screenshot") } );
     $("#dialog_worldcup").dialog("open");
+  });
+  $("#dialog_worldcup").on('mouseleave', function(){
+    $("#dialog_worldcup").dialog("close");
   });
 
 
